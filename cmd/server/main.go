1c0df2c09f164c28921aee971ad99e4ba0abf135
Vladimir Krasilnikov <vladimir@krasilnikov.dev>
2026-07-12 22:44:10 +0300

    iter6: use logging middleware for server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"errors"
 	"flag"
-	"log"
 	"net/http"
 	"os"
 	"os/signal"
@@ -14,7 +13,47 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/rebusman/svcmetrics/internal/handler"
 	"github.com/rebusman/svcmetrics/internal/storage"
+	"github.com/sirupsen/logrus"
 )
+
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+	bodySize   int
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
+}
+
+func (rw *responseWriter) Write(b []byte) (int, error) {
+	size, err := rw.ResponseWriter.Write(b)
+	rw.bodySize += size
+	return size, err
+}
+
+func loggingMiddleware(log *logrus.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+
+			rw := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+
+			next.ServeHTTP(rw, r)
+
+			duration := time.Since(start)
+
+			log.WithFields(logrus.Fields{
+				"uri":         r.RequestURI,
+				"method":      r.Method,
+				"duration":    duration,
+				"status_code": rw.statusCode,
+				"body_size":   rw.bodySize,
+			}).Info("Request handled")
+		})
+	}
+}
 
 func main() {
 	addr := flag.String("a", "localhost:8080", "address and port to run server")
@@ -24,12 +63,19 @@ func main() {
 		*addr = envAddr
 	}
 
+	log := logrus.New()
+	log.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+	})
+
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
 	s := storage.NewMemStorage()
 
 	r := chi.NewRouter()
+	r.Use(loggingMiddleware(log))
+
 	r.Post("/update/{type}/{name}/{value}", handler.UpdateHandler(s))
 	r.Get("/value/{type}/{name}", handler.ValueHandler(s))
 	r.Get("/", handler.ListHandler(s))
